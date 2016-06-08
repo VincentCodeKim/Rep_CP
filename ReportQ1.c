8a6e88b5037bef2af0af04a68f1fa72e4e893dcf
VincentCode <calla987@gmail.com>
2016-06-08 20:54:41 +0900

    ㄴㅇㄹㅇㄴㄹㄴ

--- ReportQ1.c
+++ ReportQ1.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h> //표준입출력선행처리기
 int main(void) { // 메인함수호출
 	char op;              // 문자형op 변수선언
@@ -12,27 +11,22 @@ int main(void) { // 메인함수호출
 	if (op == '+') {     //만약op값이+이면..
 
 		result = x + y; //result 변수에x+ y값(합)을대입
-
 	}
 	else if (op == '-') { //만약op값이-이면..
 
 		result = x - y; //result 변수에x- y값(빼기)을대입
-
 	}
 	else if (op == '*') { //만약op값이*이면..
 
 		result = x * y;  //result변수에x * y값(곱하기)을대입
-
 	}
 	else if (op == '/') {  //만약op값이/이면..
 
 		result = x / y;  //result변수에x / y값(나누기)을대입
-
 	}
 	else if (op == '%') {  //만약op값이%이면..
 
 		result = x % y;  //result변수에x - y값(나머지값)을대입
-
 	}
 	else {  //위의if나else if 조건에해당되지않으면..
 
@@ -42,5 +36,5 @@ int main(void) { // 메인함수호출
 	printf("%d%c %d =%d \n", x, op, y, result);
 
 	//x(정수형) op(문자형) y(정수형) = result(정수형)값출력
-	return0; //메인함수종료
+	return 0; //메인함수종료
 }
